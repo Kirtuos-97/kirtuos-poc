e3f142a3782203a8e0765c0fbad947c1d676677f
Kirtuos <soutrikmkhj@gmail.com>
2026-09-05 14:58:26 +0530

    added the logistic_reg model

---
+++ code-repository/big-query/models/predict_customer_transaction.sql
@@ -0,0 +1,21 @@
+--LOGISTIC_REG
+CREATE OR REPLACE MODEL `${project_id}.${ml_dataset_id}.predict_customer_transaction`
+OPTIONS(
+  model_type = 'LOGISTIC_REG',
+  input_label_cols = ['label'],
+  auto_class_weights = TRUE,
+  data_split_method = 'AUTO_SPLIT'
+) AS
+SELECT
+  label,
+  pageviews,
+  hits,
+  bounces,
+  time_on_site,
+  visit_number,
+  os,
+  is_mobile,
+  country,
+  channel_grouping
+FROM
+  `${project_id}.${dataset_id}.ga_logistic_features`;
