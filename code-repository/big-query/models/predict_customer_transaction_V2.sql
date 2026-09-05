52fc37f1934c329b20506affae0145a850bc3a23
Kirtuos <soutrikmkhj@gmail.com>
2026-09-05 15:58:02 +0530

    added V2 of Predict_customer_transaction model

---
+++ code-repository/big-query/models/predict_customer_transaction_V2.sql
@@ -0,0 +1,25 @@
+--LOGISTIC_REG
+CREATE OR REPLACE MODEL `${project_id}.${ml_dataset_id}.predict_customer_transaction_V2`
+OPTIONS(
+  model_type = 'LOGISTIC_REG',
+  input_label_cols = ['label'],
+  auto_class_weights = FALSE,
+  class_weights=[STRUCT('0' AS label, 1.0 AS weight), STRUCT('1' AS label, 5.0 AS weight)],
+  data_split_method = 'AUTO_SPLIT'
+) AS
+SELECT
+  label,
+  pageviews,
+  hits,
+  bounces,
+  time_on_site,
+  visit_number,
+  os,
+  is_mobile,
+  country,
+  channel_grouping
+FROM
+  `${project_id}.${dataset_id}.ga_logistic_features`
+WHERE
+  visit_date between '2016-08-01' and '2016-12-31'
+  ;
